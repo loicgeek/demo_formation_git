7b3807dbc207612f498577fff2db755686459345
loicgeek <loic.ngou98@gmail.com>
2022-02-12 16:03:17 +0100

    Added listView

--- lib/src/conversation_details.dart
+++ lib/src/conversation_details.dart
@@ -12,10 +12,27 @@ class ConversationDetails extends StatefulWidget {
 
 class _ConversationDetailsState extends State<ConversationDetails> {
   late TextEditingController _messageController;
+  bool _isEditing = false;
+  List<Message> _messages = [];
   @override
   void initState() {
     super.initState();
     _messageController = TextEditingController();
+    initMessages();
+  }
+
+  initMessages() {
+    for (var i = 0; i < 10; i++) {
+      _messages.add(
+        Message(
+          i,
+          'Bonjour $i, comment tu vas ?',
+          DateTime.now().add(
+            Duration(minutes: i),
+          ),
+        ),
+      );
+    }
   }
 
   @override
@@ -26,62 +43,140 @@ class _ConversationDetailsState extends State<ConversationDetails> {
       ),
       body: Column(
         children: [
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: TextFormField(
-                    controller: _messageController,
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.all(5),
-                      hintText: 'Message',
-                      prefixIcon: Icon(
-                        Icons.sms_outlined,
-                      ),
-                      suffixIcon: Padding(
-                        padding: const EdgeInsets.only(right: 10.0),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
+          Expanded(
+            child: ListView.builder(
+              reverse: true,
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                var message = _messages[index];
+                return Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: index % 3 == 0
+                      ? MainAxisAlignment.end
+                      : MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        constraints: BoxConstraints(
+                            maxWidth: MediaQuery.of(context).size.width * .7),
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.deepPurple,
+                        ),
+                        child: Column(
                           children: [
-                            Transform.rotate(
-                              angle: pi / 4,
-                              child: Icon(Icons.attachment),
-                            ),
-                            Icon(
-                              Icons.photo_camera,
+                            Text(
+                              message.message,
+                              style: TextStyle(color: Colors.white),
                             ),
                           ],
                         ),
                       ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
+                    )
+                  ],
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextFormField(
+                      controller: _messageController,
+                      minLines: 1,
+                      maxLines: 3,
+                      onChanged: (value) {
+                        if (value.isNotEmpty) {
+                          setState(() {
+                            _isEditing = true;
+                          });
+                        } else {
+                          setState(() {
+                            _isEditing = false;
+                          });
+                        }
+                      },
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(5),
+                        hintText: 'Message',
+                        prefixIcon: Icon(
+                          Icons.sms_outlined,
+                        ),
+                        suffixIcon: _isEditing
+                            ? Padding(
+                                padding: const EdgeInsets.only(right: 10.0),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Transform.rotate(
+                                      angle: pi / 4,
+                                      child: Icon(Icons.attachment),
+                                    ),
+                                    Icon(
+                                      Icons.photo_camera,
+                                    ),
+                                  ],
+                                ),
+                              )
+                            : null,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  print(_messageController.text);
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: const BoxDecoration(
-                    color: Colors.deepPurple,
-                    shape: BoxShape.circle,
+                Padding(
+                  padding: const EdgeInsets.only(right: 5.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      print(_messageController.text);
+                      if (_messageController.text.isNotEmpty) {
+                        _messages.insert(
+                          0,
+                          Message(
+                            _messages.length,
+                            _messageController.text,
+                            DateTime.now(),
+                          ),
+                        );
+                        _messageController.clear();
+                        setState(() {});
+                      }
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: const BoxDecoration(
+                        color: Colors.deepPurple,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Center(
+                          child: Icon(
+                        _isEditing ? Icons.send : Icons.mic,
+                        color: Colors.white,
+                      )),
+                    ),
                   ),
-                  child: const Center(
-                      child: Icon(
-                    Icons.send,
-                    color: Colors.white,
-                  )),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
     );
   }
+}
+
+class Message {
+  final int id;
+  final String message;
+  final DateTime date;
+
+  Message(this.id, this.message, this.date);
 }
